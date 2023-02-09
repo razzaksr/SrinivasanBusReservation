5b3c855095ffcec23acccb23b76a3c551562d307
razzaksr <razzaksr@gmail.com>
2023-02-10 01:47:32 +0530

    Reservation Console App with MS SQL

--- tables.sql
+++ tables.sql
@@ -1,5 +1,5 @@
 Creating Bus table
-create table bus(id int not null primary key, name varchar, capacity int);
+create table bus(id int not null primary key, name varchar(MAX), capacity int);
 
 Creating passengers table
-create table passengers(sno int not null identity(1,1) primary key, age int not null, name varchar, doj date, bus_id int foreign key references bus(id));
+create table passengers(sno int not null identity(1,1) primary key, age int not null, name varchar(MAX), doj date, bus_id int foreign key references bus(id));
