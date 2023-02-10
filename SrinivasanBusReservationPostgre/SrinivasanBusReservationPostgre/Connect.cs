a04f7dba920a7522c66314dadc0c6526ed2e4c4f
razzaksr <razzaksr@gmail.com>
2023-02-10 10:41:35 +0530

    Complete reservation system using postgresql done

--- SrinivasanBusReservationPostgre/SrinivasanBusReservationPostgre/Connect.cs
+++ SrinivasanBusReservationPostgre/SrinivasanBusReservationPostgre/Connect.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Data.SqlClient;
 using System.Xml.Linq;
 using ConsoleTables;
 using System.Security.Cryptography;
@@ -20,14 +19,10 @@ namespace SrinivasanBusReservationPostgre
         public String name;
         public DateTime date;
         public int capacity;
-        public int availablity;
+        public Int64 availablity;
     }
     internal class Connect
     {
-        SqlConnection _connection;
-        SqlCommand _command;
-        SqlDataReader _reader;
-
         NpgsqlConnection npgsqlConnection;
         NpgsqlCommand npgsqlCommand;
         NpgsqlDataReader npgsqlDataReader;
@@ -44,17 +39,17 @@ namespace SrinivasanBusReservationPostgre
             Console.WriteLine("Enter the date of journey");
             DateTime dt = Convert.ToDateTime(Console.ReadLine());
             Console.WriteLine(dt.Date);
-            _connection = new SqlConnection("Data Source=SRDB\\SQLEXPRESS;Initial Catalog=booking;Integrated Security=True");
-            _command = new SqlCommand("select * from passengers where bus_id=@i and doj=@d", _connection);
-            _command.Parameters.AddWithValue("@i", bid);
-            _command.Parameters.AddWithValue("@d", dt.Date);
-            _connection.Open();
-            _reader = _command.ExecuteReader();
-            while (_reader.Read())
+            npgsqlConnection = new NpgsqlConnection(@"Server=localhost;Port=5432;User Id=postgres;Password=admin;Database=booking");
+            npgsqlCommand = new NpgsqlCommand("select * from passengers where bus_id=@i and doj=@d", npgsqlConnection);
+            npgsqlCommand.Parameters.AddWithValue("@i", bid);
+            npgsqlCommand.Parameters.AddWithValue("@d", dt.Date);
+            npgsqlConnection.Open();
+            npgsqlDataReader = npgsqlCommand.ExecuteReader();
+            while (npgsqlDataReader.Read())
             {
-                tab.AddRow(_reader["sno"], _reader["name"], _reader["age"]);
+                tab.AddRow(npgsqlDataReader["sno"], npgsqlDataReader["name"], npgsqlDataReader["age"]);
             }
-            _connection.Close();
+            npgsqlConnection.Close();
             tab.Write();
         }
         public void Availability()
@@ -63,69 +58,39 @@ namespace SrinivasanBusReservationPostgre
             Console.WriteLine("Enter the date of journey");
             DateTime dt = Convert.ToDateTime(Console.ReadLine());
             Console.WriteLine(dt.Date);
-            _connection = new SqlConnection("Data Source=SRDB\\SQLEXPRESS;Initial Catalog=booking;Integrated Security=True");
-            _command = new SqlCommand("select distinct doj,bus.id,bus.name,bus.capacity from passengers inner join bus on bus_id=bus.id where doj=@d", _connection);
-            _command.Parameters.AddWithValue("@d", dt.Date);
-            _connection.Open();
-            _reader = _command.ExecuteReader();
+            npgsqlConnection = new NpgsqlConnection(@"Server=localhost;Port=5432;User Id=postgres;Password=admin;Database=booking");
+            npgsqlCommand = new NpgsqlCommand("select distinct doj,bus.id,bus.name,bus.capacity from passengers inner join bus on bus_id=bus.id where doj=@d", npgsqlConnection);
+            npgsqlCommand.Parameters.AddWithValue("@d", dt.Date);
+            npgsqlConnection.Open();
+            npgsqlDataReader = npgsqlCommand.ExecuteReader();
             ArrayList ar = new ArrayList();
-            while (_reader.Read())
+            while (npgsqlDataReader.Read())
             {
                 Avail a = new Avail() 
                 { 
-                    busId =(int)_reader[1],
-                    name=(String)_reader[2],
-                    date=(DateTime)_reader[0],
-                    capacity=(int)_reader[3],
+                    busId =(int)npgsqlDataReader[1],
+                    name=(String)npgsqlDataReader[2],
+                    date=(DateTime)npgsqlDataReader[0],
+                    capacity= (int)npgsqlDataReader[3],
                 };
                 ar.Add(a);
             }
-            _connection.Close();
+            npgsqlConnection.Close();
             for(int index=0;index<ar.Count;index++)
             {
                 Avail a = (Avail) ar[index];
-                _command = new SqlCommand("select count(*) from passengers where bus_id=@i and doj=@d", _connection);
-                _command.Parameters.AddWithValue("@i", a.busId);
-                _command.Parameters.AddWithValue("@d", a.date);
-                _connection.Open();
-                _reader = _command.ExecuteReader();
-                if (_reader.Read())
+                npgsqlCommand = new NpgsqlCommand("select count(*) from passengers where bus_id=@i and doj=@d", npgsqlConnection);
+                npgsqlCommand.Parameters.AddWithValue("@i", a.busId);
+                npgsqlCommand.Parameters.AddWithValue("@d", a.date);
+                npgsqlConnection.Open();
+                npgsqlDataReader = npgsqlCommand.ExecuteReader();
+                if (npgsqlDataReader.Read())
                 {
-                    a.availablity = a.capacity - (int)_reader[0];
+                    a.availablity = a.capacity - (Int64)npgsqlDataReader[0];
                     tab.AddRow(a.busId, a.name, a.date, a.capacity, a.availablity);
-                    //int asNow = (int)_reader[0];
-                    //tab.AddRow(rows[0, 0], _reader[2], _reader[0], _reader[3], (int)_reader[3] - asNow);
                 }
             }
             tab.Write();
-            /*
-            _command = new SqlCommand("select distinct doj,bus.id,bus.name,bus.capacity from passengers inner join bus on bus_id=bus.id where doj=@d", _connection);
-            //_command = new SqlCommand("select doj,bus.id,bus.name,bus.capacity from passengers inner join bus on bus_id=bus.id where doj=@d", _connection);
-            //_command = new SqlCommand("select passengers.doj,id,name,capacity from bus inner join passengers on bus.id=passengers.bus_id where doj=@d", _connection);
-            _command.Parameters.AddWithValue("@d", dt.Date);
-            _connection.Open();
-            _reader = _command.ExecuteReader();
-            if (_reader.Read())
-            {
-
-            }
-            _connection.Close();
-            */
-            /*
-            _command = new SqlCommand("select count(*) from passengers where bus_id=@i and doj=@d", _connection);
-            _command.Parameters.AddWithValue("@i", 101);
-            _command.Parameters.AddWithValue("@d", DateTime.Parse("20/02/2023"));
-            _connection.Open();
-            _reader = _command.ExecuteReader();
-            if (_reader.Read())
-            {
-                int asNow = (int)_reader[0];
-                //tab.AddRow(rows[0, 0], _reader[2], _reader[0], _reader[3], (int)_reader[3] - asNow);
-            }
-
-
-            tab.Write();
-            */
         }
         public void AddPassenger()
         {
@@ -134,40 +99,40 @@ namespace SrinivasanBusReservationPostgre
             Console.WriteLine("Enter the date of journey");
             DateTime dt = Convert.ToDateTime(Console.ReadLine());
             Console.WriteLine(dt.Date);
-            _connection = new SqlConnection("Data Source=SRDB\\SQLEXPRESS;Initial Catalog=booking;Integrated Security=True");
-            _command = new SqlCommand("select count(*) from passengers where bus_id=@i and doj=@d", _connection);
-            _command.Parameters.AddWithValue("@i", bid);
-            _command.Parameters.AddWithValue("@d", dt.Date);
-            _connection.Open();
-            _reader = _command.ExecuteReader();
-            if (_reader.Read())
+            npgsqlConnection = new NpgsqlConnection(@"Server=localhost;Port=5432;User Id=postgres;Password=admin;Database=booking");
+            npgsqlCommand = new NpgsqlCommand("select count(*) from passengers where bus_id=@i and doj=@d", npgsqlConnection);
+            npgsqlCommand.Parameters.AddWithValue("@i", bid);
+            npgsqlCommand.Parameters.AddWithValue("@d", dt.Date);
+            npgsqlConnection.Open();
+            npgsqlDataReader = npgsqlCommand.ExecuteReader();
+            if (npgsqlDataReader.Read())
             {
-                int asNow = (int)_reader[0];
-                _connection.Close();
+                Int64 asNow = (Int64)npgsqlDataReader[0];
+                npgsqlConnection.Close();
                 Console.WriteLine(asNow);
-                _command = new SqlCommand("select capacity from bus where id=@i", _connection);
-                _command.Parameters.AddWithValue("@i", bid);
-                _connection.Open();
-                _reader = _command.ExecuteReader();
-                if (_reader.Read())
+                npgsqlCommand = new NpgsqlCommand("select capacity from bus where id=@i", npgsqlConnection);
+                npgsqlCommand.Parameters.AddWithValue("@i", bid);
+                npgsqlConnection.Open();
+                npgsqlDataReader = npgsqlCommand.ExecuteReader();
+                if (npgsqlDataReader.Read())
                 {
-                    int asFact = (int)_reader["capacity"];
-                    _connection.Close();
+                    int asFact = (int)npgsqlDataReader["capacity"];
+                    npgsqlConnection.Close();
                     if (asFact > asNow)
                     {
-                        _connection.Close();
+                        npgsqlConnection.Close();
                         Console.WriteLine("Enter passenger name and age");
                         String pname = Console.ReadLine();
                         int age = Convert.ToInt32(Console.ReadLine());
-                        _command = new SqlCommand("insert into passengers(name,age,doj,bus_id) values(@n,@a,@d,@b)", _connection);
-                        _command.Parameters.AddWithValue("@n", pname);
-                        _command.Parameters.AddWithValue("@a", age);
-                        _command.Parameters.AddWithValue("@d", dt);
-                        _command.Parameters.AddWithValue("@b", bid);
-                        _connection.Open();
-                        _command.ExecuteNonQuery();
+                        npgsqlCommand = new NpgsqlCommand("insert into passengers(name,age,doj,bus_id) values(@n,@a,@d,@b)", npgsqlConnection);
+                        npgsqlCommand.Parameters.AddWithValue("@n", pname);
+                        npgsqlCommand.Parameters.AddWithValue("@a", age);
+                        npgsqlCommand.Parameters.AddWithValue("@d", dt);
+                        npgsqlCommand.Parameters.AddWithValue("@b", bid);
+                        npgsqlConnection.Open();
+                        npgsqlCommand.ExecuteNonQuery();
                         Console.WriteLine("Booking created successfully");
-                        _connection.Close();
+                        npgsqlConnection.Close();
                     }
                     else
                     {
@@ -175,51 +140,33 @@ namespace SrinivasanBusReservationPostgre
                     }
                 }
             }
-            /*
-            
-            
-            
-            /*
-            if (_reader.Read())
-            {
-                Console.WriteLine("Journey available " + _reader[0]);
-                if ((int)_reader[0] > 0)
-                {
-                    Console.WriteLine(_reader["doj"]);
-                }
-            }
-            else
-            {
-                Console.WriteLine("Journey not available start it");
-            }*/
-
         }
         public void UpdateOne(int bid)
         {
-            _connection = new SqlConnection("Data Source=SRDB\\SQLEXPRESS;Initial Catalog=booking;Integrated Security=True");
-            _command = new SqlCommand("select * from bus where id=@i", _connection);
-            _command.Parameters.AddWithValue("@i", bid);
-            _connection.Open();
-            _reader = _command.ExecuteReader();
-            if (_reader.Read())
+            npgsqlConnection = new NpgsqlConnection(@"Server=localhost;Port=5432;User Id=postgres;Password=admin;Database=booking");
+            npgsqlCommand=new NpgsqlCommand("select * from bus where id=@i", npgsqlConnection);
+            npgsqlCommand.Parameters.AddWithValue("@i", bid);
+            npgsqlConnection.Open();
+            npgsqlDataReader = npgsqlCommand.ExecuteReader();
+            if (npgsqlDataReader.Read())
             {
-                bid= (int)_reader["id"];
-                String nm =(String) _reader["name"];
-                int cap = (int)_reader["capacity"];
+                bid= (int)npgsqlDataReader["id"];
+                String nm =(String)npgsqlDataReader["name"];
+                int cap = (int)npgsqlDataReader["capacity"];
                 Console.WriteLine("Found " + bid + " and " + nm + " with " + cap);
-                _connection.Close();
+                npgsqlConnection.Close();
                 Console.WriteLine("Enter the new name to " + bid);
                 nm=Console.ReadLine();
                 Console.WriteLine("Enter the new Capacity to " + bid);
                 cap=Convert.ToInt32(Console.ReadLine());
-                _command = new SqlCommand("update bus set name=@n, capacity=@c where id=@i", _connection);
-                _command.Parameters.AddWithValue("@i", bid);
-                _command.Parameters.AddWithValue("@n", nm);
-                _command.Parameters.AddWithValue("@c", cap);
-                _connection.Open();
-                _command.ExecuteNonQuery();
+                npgsqlCommand = new NpgsqlCommand("update bus set name=@n, capacity=@c where id=@i", npgsqlConnection);
+                npgsqlCommand.Parameters.AddWithValue("@i", bid);
+                npgsqlCommand.Parameters.AddWithValue("@n", nm);
+                npgsqlCommand.Parameters.AddWithValue("@c", cap);
+                npgsqlConnection.Open();
+                npgsqlCommand.ExecuteNonQuery();
                 Console.WriteLine("Record has updated");
-                _connection.Close();
+                npgsqlConnection.Close();
             }
             else
             {
@@ -228,18 +175,18 @@ namespace SrinivasanBusReservationPostgre
         }
         public void DetailAll()
         {
-            _connection = new SqlConnection("Data Source=SRDB\\SQLEXPRESS;Initial Catalog=booking;Integrated Security=True");
-                _command = new SqlCommand("select * from bus", _connection);
-                _connection.Open();
-                _reader = _command.ExecuteReader();
+            npgsqlConnection = new NpgsqlConnection(@"Server=localhost;Port=5432;User Id=postgres;Password=admin;Database=booking");
+            npgsqlCommand = new NpgsqlCommand(@"select * from bus", npgsqlConnection);
+            npgsqlConnection.Open();
+            npgsqlDataReader = npgsqlCommand.ExecuteReader();
             var tab = new ConsoleTable("Bus ID", "Bus Name", "Bus Capacity");
-                while (_reader.Read())
+                while (npgsqlDataReader.Read())
                 {
                 //Console.WriteLine(_reader["id"] + " " + _reader["name"] + " " + _reader["capacity"]);
-                tab.AddRow(_reader["id"], _reader["name"], _reader["capacity"]);
+                tab.AddRow(npgsqlDataReader["id"], npgsqlDataReader["name"], npgsqlDataReader["capacity"]);
                 }
             tab.Write();
-            _connection.Close();
+            npgsqlConnection.Close();
         }
 
         public void Insert(int bid,String bname,int bcap)
@@ -258,7 +205,7 @@ namespace SrinivasanBusReservationPostgre
 
         ~Connect()
         {
-            _connection.Close();
+            npgsqlConnection.Close();
         }
     }
 }
--- SrinivasanBusReservationPostgre/SrinivasanBusReservationPostgre/Program.cs
+++ SrinivasanBusReservationPostgre/SrinivasanBusReservationPostgre/Program.cs
@@ -3,5 +3,72 @@ using SrinivasanBusReservationPostgre;
 
 Console.WriteLine("Hello, World!");
 
-Connect connect = new Connect();
-connect.Insert(101,"SRSDLX",10);
+Connect con = new Connect();
+
+home:
+    do
+    {
+        Console.WriteLine("Enter 1.Admin\n2.User");
+        int who = Convert.ToInt32(Console.ReadLine());
+        switch (who)
+        {
+            case 1:
+                do
+                {
+                    Console.WriteLine("Enter 1. Add new bus\n2. To view list\n3. To update bus\n4. To print passengers list\n5. For availability check\n9. To go back");
+                    int adOpt = Convert.ToInt32(Console.ReadLine());
+                    switch (adOpt)
+                    {
+                        case 1:
+                            Console.WriteLine("Enter Bus Id");
+                            int i = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Enter Bus Name");
+                            String n = Console.ReadLine();
+                            Console.WriteLine("Enter Bus Capacity");
+                            int c = Convert.ToInt32(Console.ReadLine());
+                            con.Insert(i, n, c);
+                            break;
+                        case 2:
+                            con.DetailAll();
+                            break;
+                        case 3:
+                            Console.WriteLine("Enter the Bus Id");
+                            int b = Convert.ToInt32(Console.ReadLine());
+                            con.UpdateOne(b);
+                            break;
+                        case 4:
+                            con.ViewPassenger();
+                            break;
+                        case 5:
+                            con.Availability();
+                            break;
+                        default:
+                            goto home;
+                    }
+                } while (true);
+            case 2:
+                do
+                {
+                    Console.WriteLine("Enter 1. For availability check\n2. For booking tickets\n9. To go back");
+                    int usrOpt = Convert.ToInt32(Console.ReadLine());
+                    switch (usrOpt)
+                    {
+                        case 1:
+                            con.Availability();
+                            break;
+                        case 2:
+                            con.AddPassenger();
+                            break;
+                        default: goto home;
+                    }
+                } while (true);
+            default: return;
+        }
+    } while (true);
+
+//connect.Insert(101,"SRSDLX",10);
+//connect.AddPassenger();
+//connect.DetailAll();
+//connect.UpdateOne(101);
+//connect.Availability();
+//connect.ViewPassenger();
